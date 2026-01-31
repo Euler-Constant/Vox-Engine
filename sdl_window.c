f8e619f57c93aee1091f6eecbd95d0e7414e408e
Euler-Constant <davidorifa39@gmail.com>
2026-01-31 19:40:57 +0100

    sddl window initialization

---
+++ sdl_window.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+int int main(int argc, char *argv[])
+{
+
+  if((SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO)==-1)) 
+  { 
+    printf("Could not initialize SDL: %s.\n", SDL_GetError());
+    exit(-1);
+  }
+  else {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+      printf("SDL init failed: %s\n", SDL_GetError());
+      return 1;
+    };
+  };
+
+  SDL_Window* window = SDL_CreateWindow("Vox-Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN)
+  if (!window) {
+    printf("Window creation failed: %s\n", SDL_GetError());
+    SDL_Quit();
+    return 1;
+  };
+
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  if (!renderer) {
+    printf("Renderer failed: %s\n", SDL_GetError());
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 1;
+  };
+};
+
